0ccc0e4df8db48bb97d2f2e8dddb54da05708d71
Fatih <fatihcan.social@gmail.com>
2020-06-21 18:51:40 +0300

    add mobile nav state

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/dashboard/layout'
 import Sidebar from '../components/dashboard/sidebar'
 import Main from '../components/dashboard/main'
 import Navbar from '../components/dashboard/navbar'
 import Content from '../components/dashboard/content'
 
-const Dashboard = () => (
-  <Layout>
-    <Sidebar />
-    <Main>
-      <Navbar />
-      <Content />
-    </Main>
-  </Layout>
-)
+const Dashboard = () => {
+  const [navOpen, setNavOpen] = useState(false)
+
+  return (
+    <Layout>
+      <Sidebar navOpen={navOpen} onClose={() => setNavOpen(false)} />
+      <Main>
+        <Navbar onClick={() => setNavOpen(true)} />
+        <Content />
+      </Main>
+    </Layout>
+  )
+}
 
 export default Dashboard
