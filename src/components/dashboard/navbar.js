f9af78b9c3a4776ab3e9b3015769b0c8c17da895
Fatih <fatihcan.social@gmail.com>
2020-06-21 15:57:49 +0300

    finish navbar

--- src/components/dashboard/navbar.js
+++ src/components/dashboard/navbar.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import styles from '../../styles/navbar.module.css'
+import styles from '../../styles/dashboard/navbar.module.css'
+import NavbarSearch from './navbar-search'
+import Avatar from './avatar'
+import Bell from './icons/bell'
 
-const Navbar = () => <nav className={styles.navbar}>Navbar</nav>
+const Navbar = () => (
+  <nav className={styles.navbar}>
+    <NavbarSearch />
+    <div className={styles.right}>
+      <Bell />
+      <Avatar />
+    </div>
+  </nav>
+)
 
 export default Navbar
