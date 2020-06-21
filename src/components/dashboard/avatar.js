73b2e0261e115a6a7075cb5c215c768eb3096c3e
Fatih <fatihcan.social@gmail.com>
2020-06-21 15:59:03 +0300

    create avatar

---
+++ src/components/dashboard/avatar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../../styles/dashboard/navbar.module.css'
+
+const Avatar = () => (
+  <div className={styles.avatar}>
+    <img
+      src="https://randomuser.me/api/portraits/women/44.jpg"
+      alt="blonde short haired woman"
+    />
+  </div>
+)
+
+export default Avatar
