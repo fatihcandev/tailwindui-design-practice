07c712b015d700df417b47414cbef36a788f4730
Fatih <fatihcan.social@gmail.com>
2020-06-21 18:51:51 +0300

    create dashed area

---
+++ src/components/dashboard/dashed-area.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../../styles/dashboard/content.module.css'
+
+const DashedArea = () => <div className={styles.dashedArea} />
+
+export default DashedArea
