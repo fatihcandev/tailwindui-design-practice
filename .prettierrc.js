76862fa3f6dc819741a38a6ed34c48df71453af6
Fatih <fatihcan.social@gmail.com>
2020-06-21 12:25:59 +0300

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  arrowParens: 'avoid',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false
+}
