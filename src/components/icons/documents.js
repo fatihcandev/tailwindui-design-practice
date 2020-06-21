8366660dedb16e104d084f84aab88d7805f24820
Fatih <fatihcan.social@gmail.com>
2020-06-21 14:14:46 +0300

    add icons

---
+++ src/components/icons/documents.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Documents = () => (
+  <svg
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path>
+  </svg>
+)
+
+export default Documents
