2a2f5eb813cb6aac4c16033cfc77f1cdc29c6648
Fatih <fatihcan.social@gmail.com>
2020-06-21 14:18:24 +0300

    create sidebar item

---
+++ src/components/sidebar-item.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/sidebar.module.css'
+
+const SidebarItem = ({ children, ...props }) => (
+  <div className={styles.sidebarItem} {...props}>
+    {children}
+  </div>
+)
+
+export default SidebarItem
