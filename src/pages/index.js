c869aa3a3b3c4576dac54ba2f7aa0367b003ddcc
Fatih <fatihcan.social@gmail.com>
2020-06-21 15:02:23 +0300

    create welcome page and styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,15 @@
 import React from 'react'
-import Layout from '../components/layout'
-import Sidebar from '../components/sidebar'
-import Main from '../components/main'
-import Navbar from '../components/navbar'
-import Content from '../components/content'
+import TextBody from '../components/dashboard/text-body'
+import { Link } from 'gatsby'
+import styles from '../styles/index.module.css'
 
 export default function Index() {
   return (
-    <Layout>
-      <Sidebar />
-      <Main>
-        <Navbar />
-        <Content />
-      </Main>
-    </Layout>
+    <div className={styles.index}>
+      <TextBody>Welcome!</TextBody>
+      <Link to="/dashboard">
+        <TextBody>Go to dashboard</TextBody>
+      </Link>
+    </div>
   )
 }
