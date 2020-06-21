5360865fccdd0a9b632aa63635389c466555a8ab
Fatih <fatihcan.social@gmail.com>
2020-06-21 14:17:53 +0300

    add layout to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
-import React from "react"
+import React from 'react'
+import Layout from '../components/layout'
+import Sidebar from '../components/sidebar'
+import Main from '../components/main'
+import Navbar from '../components/navbar'
+import Content from '../components/content'
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Index() {
+  return (
+    <Layout>
+      <Sidebar />
+      <Main>
+        <Navbar />
+        <Content />
+      </Main>
+    </Layout>
+  )
 }
