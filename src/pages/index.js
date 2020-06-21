d3a9e3b4bd083561dea6774268a2d89438c01a86
Fatih <fatihcan.social@gmail.com>
2020-06-21 15:57:29 +0300

    edit index page and add styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import TextBody from '../components/dashboard/text-body'
-import { Link } from 'gatsby'
 import styles from '../styles/index.module.css'
+import Navigator from '../components/navigator'
+import TailwindUILogo from '../components/tailwind-ui-logo'
 
 export default function Index() {
   return (
     <div className={styles.index}>
-      <TextBody>Welcome!</TextBody>
-      <Link to="/dashboard">
-        <TextBody>Go to dashboard</TextBody>
-      </Link>
+      <a href="https://tailwindui.com" title="Go to Tailwind UI">
+        <TailwindUILogo />
+      </a>
+      <TextBody>
+        Welcome. Here is the list of the user interfaces I've re-designed from
+        Tailwind UI for the purpose of practice
+      </TextBody>
+      <Navigator to="/dashboard">Brand sidebar with light header</Navigator>
     </div>
   )
 }
