a8516becf53b3929d34bbf55de14c5ba2058731b
Fatih <fatihcan.social@gmail.com>
2020-06-21 18:53:06 +0300

    add menu icon

---
+++ src/components/dashboard/icons/menu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../../../styles/dashboard/navbar.module.css'
+
+const Menu = ({ onClick }) => (
+  <button className={styles.menu} onClick={onClick}>
+    <svg
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path d="M4 6h16M4 12h8m-8 6h16"></path>
+    </svg>
+  </button>
+)
+
+export default Menu
