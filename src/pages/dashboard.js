dd697696c32657c3ee66a66ec8dce56aae030556
Fatih <fatihcan.social@gmail.com>
2020-06-23 18:04:10 +0300

    add seo component

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -4,12 +4,14 @@ import Sidebar from '../components/dashboard/sidebar'
 import Main from '../components/dashboard/main'
 import Navbar from '../components/dashboard/navbar'
 import Content from '../components/dashboard/content'
+import SEO from '../components/seo'
 
 const Dashboard = () => {
   const [navOpen, setNavOpen] = useState(false)
 
   return (
     <Layout>
+      <SEO title="Brand sidebar with light header" />
       <Sidebar navOpen={navOpen} onClose={() => setNavOpen(false)} />
       <Main>
         <Navbar onClick={() => setNavOpen(true)} />
