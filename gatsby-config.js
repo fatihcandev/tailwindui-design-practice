2b679837c49b137bfb2c57a72070e0d692f9311e
Fatih <fatihcan.social@gmail.com>
2020-06-23 10:56:52 +0300

    add plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`
+      }
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`
+  ]
 }
