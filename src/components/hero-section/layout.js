66eb93b4137cf7604824963e837ec11543eeafe8
Fatih <fatihcan.social@gmail.com>
2020-06-23 10:55:49 +0300

    create hero section layout and styles

---
+++ src/components/hero-section/layout.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../../styles/hero-section/layout.module.css'
+
+const Layout = ({ children }) => <div className={styles.layout}>{children}</div>
+
+export default Layout
