cd544fed07fb0b0a7814f28b06af415f9bfd866e
Fatih <fatihcan.social@gmail.com>
2020-06-23 18:04:34 +0300

    add title prop

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -4,10 +4,10 @@ import { Link } from 'gatsby'
 import styles from '../styles/navigator.module.css'
 import ArrowRight from './arrow'
 
-const Navigator = ({ to, children }) => (
+const Navigator = ({ to, title, children }) => (
   <div className={styles.navigator}>
     <ArrowRight />
-    <Link to={to}>
+    <Link to={to} title={title}>
       <TextBody>{children}</TextBody>
     </Link>
   </div>
