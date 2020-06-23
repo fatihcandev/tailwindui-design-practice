0751511f385eb82b6ed285d19b590d4bdd85c41e
Fatih <fatihcan.social@gmail.com>
2020-06-23 18:04:22 +0300

    add seo component and title to links

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import TextBody from '../components/text-body'
 import styles from '../styles/index.module.css'
 import Navigator from '../components/navigator'
 import TailwindUILogo from '../components/tailwind-ui-logo'
+import SEO from '../components/seo'
 
 export default function Index() {
   return (
     <div className={styles.index}>
+      <SEO />
       <div className={styles.container}>
         <a href="https://tailwindui.com" title="Go to Tailwind UI">
           <TailwindUILogo />
@@ -16,8 +18,15 @@ export default function Index() {
           Tailwind UI for the purpose of practice
         </TextBody>
         <div className={styles.links}>
-          <Navigator to="/dashboard">Brand sidebar with light header</Navigator>
-          <Navigator to="/hero-section">Hero section</Navigator>
+          <Navigator to="/dashboard" title="Brand sidebar with light header">
+            Brand sidebar with light header
+          </Navigator>
+          <Navigator
+            to="/hero-section"
+            title="Hero section with angled image on right"
+          >
+            Hero section with angled image on right
+          </Navigator>
         </div>
       </div>
     </div>
