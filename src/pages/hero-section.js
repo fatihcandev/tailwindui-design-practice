9a892f0df4daf00bc0d04e76eb8ef9675cb63afd
Fatih <fatihcan.social@gmail.com>
2020-06-23 17:18:49 +0300

    add call to action section, add alt to the image

--- src/pages/hero-section.js
+++ src/pages/hero-section.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/hero-section/layout'
 import CallToAction from '../components/hero-section/call-to-action'
 import ImageSection from '../components/hero-section/image-section'
 import Image from '../components/image'
-import { graphql } from 'gatsby'
 import Navbar from '../components/hero-section/navbar'
 import SkewedShape from '../components/hero-section/skewed-container'
 import MobileNav from '../components/hero-section/mobile-navbar'
+import TextBody from '../components/text-body'
+import styles from '../styles/hero-section/call-to-action.module.css'
+import TextTitle from '../components/text-title'
+import ThemeButton from '../components/theme-button'
 
 const HeroSection = ({ data }) => {
   const [navOpen, setNavOpen] = useState(false)
@@ -17,9 +21,27 @@ const HeroSection = ({ data }) => {
       <CallToAction>
         <Navbar onClick={() => setNavOpen(true)} />
         <MobileNav navOpen={navOpen} onClick={() => setNavOpen(false)} />
+        <div>
+          <div className={styles.textArea}>
+            <TextTitle>Data to enrich your</TextTitle>
+            <TextTitle>online business</TextTitle>
+          </div>
+          <TextBody className={styles.paragraph}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            congue quis mauris quis tincidunt. Pellentesque condimentum justo et
+            maximus auctor.
+          </TextBody>
+          <div className={styles.buttons}>
+            <ThemeButton className={styles.dark}>Get Started</ThemeButton>
+            <ThemeButton className={styles.light}>Live Demo</ThemeButton>
+          </div>
+        </div>
       </CallToAction>
       <ImageSection>
-        <Image img={data.file.childImageSharp.fluid} />
+        <Image
+          img={data.file.childImageSharp.fluid}
+          alt="people working on their computers"
+        />
       </ImageSection>
     </Layout>
   )
