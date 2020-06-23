60c866ca98dc6d5980e94d51a1a9a10ebbe08394
Fatih <fatihcan.social@gmail.com>
2020-06-23 14:23:17 +0300

    add mobile navbar and styles

---
+++ src/components/hero-section/mobile-navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import TextBody from '../text-body'
+import Logo from './icons/logo'
+import styles from '../../styles/hero-section/mobile-nav.module.css'
+import Close from './icons/close'
+
+const MobileNav = ({ navOpen, onClick }) => (
+  <div className={navOpen ? styles.mobileNavContainer : styles.mobileNav}>
+    <div className={styles.mobileNavOpen}>
+      <div className={styles.mobileNavHeader}>
+        <Logo />
+        <Close onClick={onClick} />
+      </div>
+      <TextBody>Product</TextBody>
+      <TextBody>Features</TextBody>
+      <TextBody>Marketplace</TextBody>
+      <TextBody>Company</TextBody>
+      <TextBody>Login</TextBody>
+    </div>
+  </div>
+)
+
+export default MobileNav
