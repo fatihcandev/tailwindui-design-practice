9992d1b8628ae97827bcfccab44a6380385adb29
Fatih <fatihcan.social@gmail.com>
2020-06-23 14:22:22 +0300

    add mobile nav and nav open/close state

--- src/pages/hero-section.js
+++ src/pages/hero-section.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/hero-section/layout'
 import CallToAction from '../components/hero-section/call-to-action'
 import ImageSection from '../components/hero-section/image-section'
 import Image from '../components/image'
 import { graphql } from 'gatsby'
 import Navbar from '../components/hero-section/navbar'
+import SkewedShape from '../components/hero-section/skewed-container'
+import MobileNav from '../components/hero-section/mobile-navbar'
 
 const HeroSection = ({ data }) => {
+  const [navOpen, setNavOpen] = useState(false)
+
   return (
     <Layout>
+      <SkewedShape />
       <CallToAction>
-        <Navbar />
+        <Navbar onClick={() => setNavOpen(true)} />
+        <MobileNav navOpen={navOpen} onClick={() => setNavOpen(false)} />
       </CallToAction>
       <ImageSection>
         <Image img={data.file.childImageSharp.fluid} />
