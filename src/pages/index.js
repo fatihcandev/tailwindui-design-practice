55da83786f2615fe52f6985b331596b86915576c
Fatih <fatihcan.social@gmail.com>
2020-06-23 10:56:10 +0300

    add navigator to hero section. add grid layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,14 +7,19 @@ import TailwindUILogo from '../components/tailwind-ui-logo'
 export default function Index() {
   return (
     <div className={styles.index}>
-      <a href="https://tailwindui.com" title="Go to Tailwind UI">
-        <TailwindUILogo />
-      </a>
-      <TextBody>
-        Welcome. Here is the list of the user interfaces I've re-designed from
-        Tailwind UI for the purpose of practice
-      </TextBody>
-      <Navigator to="/dashboard">Brand sidebar with light header</Navigator>
+      <div className={styles.container}>
+        <a href="https://tailwindui.com" title="Go to Tailwind UI">
+          <TailwindUILogo />
+        </a>
+        <TextBody>
+          Welcome. Here is the list of the user interfaces I've re-designed from
+          Tailwind UI for the purpose of practice
+        </TextBody>
+        <div className={styles.links}>
+          <Navigator to="/dashboard">Brand sidebar with light header</Navigator>
+          <Navigator to="/hero-section">Hero section</Navigator>
+        </div>
+      </div>
     </div>
   )
 }
