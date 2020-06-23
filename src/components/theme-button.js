b194a43613f595b9410491061699c6ea2a1cd34a
Fatih <fatihcan.social@gmail.com>
2020-06-23 18:02:58 +0300

    add ...props

--- src/components/theme-button.js
+++ src/components/theme-button.js
@@ -3,8 +3,10 @@ import Button from './button'
 import styles from '../styles/hero-section/theme-button.module.css'
 import cn from 'classnames'
 
-const ThemeButton = ({ children, className }) => (
-  <Button className={cn(styles.themeButton, className)}>{children}</Button>
+const ThemeButton = ({ children, className, ...props }) => (
+  <Button className={cn(styles.themeButton, className)} {...props}>
+    {children}
+  </Button>
 )
 
 export default ThemeButton
