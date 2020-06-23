326ceec75a026c6973d68cd13ad691db66b388ec
Fatih <fatihcan.social@gmail.com>
2020-06-23 14:20:44 +0300

    add skewed shape

---
+++ src/components/hero-section/skewed-container.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../../styles/hero-section/call-to-action.module.css'
+
+const SkewedShape = () => <div className={styles.skewedShape} />
+
+export default SkewedShape
