255e327f2c1861f75a680d189c1018bb1769520d
Fatih <fatihcan.social@gmail.com>
2020-06-24 22:57:42 +0300

    create form and styles

---
+++ src/components/sign-in-page/form.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../../styles/sign-in-page/form.module.css'
+
+const Form = ({ children }) => <div className={styles.form}>{children}</div>
+
+export default Form
