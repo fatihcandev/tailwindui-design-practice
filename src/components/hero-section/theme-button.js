563af5fc6d664c08c51bb2c94e1d5662b269ceb7
Fatih <fatihcan.social@gmail.com>
2020-06-24 23:24:18 +0300

    refactor import

--- src/components/hero-section/theme-button.js
+++ src/components/hero-section/theme-button.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Button from './button'
-import styles from '../styles/hero-section/theme-button.module.css'
+import Button from '../button'
+import styles from '../../styles/hero-section/theme-button.module.css'
 import cn from 'classnames'
 
 const ThemeButton = ({ children, className, ...props }) => (
