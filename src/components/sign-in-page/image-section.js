b911a9e7971089f8ee5ad0e0e11f887c1ddb3cd7
Fatih <fatihcan.social@gmail.com>
2020-06-24 23:21:23 +0300

    add image section and responsive styles

---
+++ src/components/sign-in-page/image-section.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../../styles/sign-in-page/image-section.module.css'
+
+const ImageSection = ({ children }) => (
+  <div className={styles.imageSection}>{children}</div>
+)
+
+export default ImageSection
