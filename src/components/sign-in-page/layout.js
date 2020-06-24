f10646ce3344d22d85dd87f9c964fa2a7b858461
Fatih <fatihcan.social@gmail.com>
2020-06-24 22:57:12 +0300

    create sign in page layout and styles

---
+++ src/components/sign-in-page/layout.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import styles from '../../styles/sign-in-page/layout.module.css'
+
+const Layout = ({ children }) => <div className={styles.layout}>{children}</div>
+
+export default Layout
