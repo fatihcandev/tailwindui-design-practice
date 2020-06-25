ef2e995b59f2e179b970bbbfdbd16cf806ec3746
Fatih <fatihcan.social@gmail.com>
2020-06-25 12:24:53 +0300

    add check icon

---
+++ src/components/sign-in-page/icons/check.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Check = () => (
+  <svg
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path d="M5 13l4 4L19 7"></path>
+  </svg>
+)
+
+export default Check
