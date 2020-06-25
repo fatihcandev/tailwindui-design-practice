dbafad63aade678d3d765425f43d856256ef048a
Fatih <fatihcan.social@gmail.com>
2020-06-25 12:25:01 +0300

    add aria label

--- src/components/sign-in-page/social-button.js
+++ src/components/sign-in-page/social-button.js
@@ -2,8 +2,10 @@ import React from 'react'
 import Button from '../button'
 import styles from '../../styles/sign-in-page/social-button.module.scss'
 
-const SocialButton = ({ children }) => (
-  <Button className={styles.socialButton}>{children}</Button>
+const SocialButton = ({ label, children }) => (
+  <Button className={styles.socialButton} aria-label={label}>
+    {children}
+  </Button>
 )
 
 export default SocialButton
