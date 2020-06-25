ea690f635d9ba8afd620d3bf9ab3d4bb43c1851b
Fatih <fatihcan.social@gmail.com>
2020-06-25 12:25:31 +0300

    add aria label

--- src/components/sign-in-page/input.js
+++ src/components/sign-in-page/input.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import styles from '../../styles/sign-in-page/input.module.css'
 
-const Input = ({ ph, type }) => (
-  <input type={type} className={styles.input} placeholder={ph} />
+const Input = ({ ph, type, label }) => (
+  <input
+    type={type}
+    className={styles.input}
+    placeholder={ph}
+    aria-label={label}
+  />
 )
 
 export default Input
