4de5e29c84b93cafe078897a3d28dabad798e9f4
Fatih <fatihcan.social@gmail.com>
2020-06-25 12:26:28 +0300

    add label to social sign in buttons, add checkbox state, add forgot password link

--- src/pages/sign-in-split-screen.js
+++ src/pages/sign-in-split-screen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/sign-in-page/layout'
 import Form from '../components/sign-in-page/form'
 import { graphql } from 'gatsby'
@@ -14,41 +14,56 @@ import GitHub from '../components/sign-in-page/icons/github'
 import Divider from '../components/sign-in-page/divider'
 import Input from '../components/sign-in-page/input'
 import ImageSection from '../components/sign-in-page/image-section'
+import SEO from '../components/seo'
+import Checkbox from '../components/sign-in-page/checkbox'
 
-const SignIn = ({ data }) => (
-  <Layout>
-    <Form>
-      <Logo />
-      <TextTitle className={styles.title}>Sign in to your account</TextTitle>
-      <div className={styles.trial}>
-        <TextBody>Or</TextBody>
-        <TextBody className={styles.blue}>
-          start your 14-day free trial
-        </TextBody>
-      </div>
-      <TextBody className={styles.grayText}>Sign in with</TextBody>
-      <div className={styles.socialButtonSection}>
-        <SocialButton>
-          <Facebook />
-        </SocialButton>
-        <SocialButton>
-          <Twitter />
-        </SocialButton>
-        <SocialButton>
-          <GitHub />
-        </SocialButton>
-      </div>
-      <Divider />
-      <TextBody className={styles.grayText}>Email Address</TextBody>
-      <Input type="email" />
-      <TextBody className={styles.grayText}>Password</TextBody>
-      <Input type="password" />
-    </Form>
-    <ImageSection>
-      <Image img={data.file.childImageSharp.fluid} alt="an architecture" />
-    </ImageSection>
-  </Layout>
-)
+const SignIn = ({ data }) => {
+  const [rememberChecked, setRememberChecked] = useState(false)
+
+  return (
+    <Layout>
+      <SEO title="Sign In Page Split Screen" />
+      <Form>
+        <Logo />
+        <TextTitle className={styles.title}>Sign in to your account</TextTitle>
+        <div className={styles.trial}>
+          <TextBody>Or</TextBody>
+          <TextBody className={styles.blue}>
+            start your 14-day free trial
+          </TextBody>
+        </div>
+        <TextBody className={styles.grayText}>Sign in with</TextBody>
+        <div className={styles.socialButtonSection}>
+          <SocialButton label="sign in with facebook">
+            <Facebook />
+          </SocialButton>
+          <SocialButton label="sign in with twitter">
+            <Twitter />
+          </SocialButton>
+          <SocialButton label="sign in with github">
+            <GitHub />
+          </SocialButton>
+        </div>
+        <Divider />
+        <TextBody className={styles.grayText}>Email Address</TextBody>
+        <Input type="email" label="email address" />
+        <TextBody className={styles.grayText}>Password</TextBody>
+        <Input type="password" label="password" />
+        <div className={styles.rememberForgot}>
+          <Checkbox
+            label="Remember me"
+            checked={rememberChecked}
+            onCheck={() => setRememberChecked(!rememberChecked)}
+          />
+          <TextBody className={styles.forgot}>Forgot your password?</TextBody>
+        </div>
+      </Form>
+      <ImageSection>
+        <Image img={data.file.childImageSharp.fluid} alt="an architecture" />
+      </ImageSection>
+    </Layout>
+  )
+}
 
 export default SignIn
 
